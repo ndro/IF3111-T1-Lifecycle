fa7f1136e0aa8ace564505880e7f96c9f048277c
ndro <hendro.tb@gmail.com>
2015-02-08 12:24:00 +0700

    Alarm finished...

---
+++ BroadcastReceiver/app/src/main/java/com/andoroid/broadcastreceiver/MyBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package com.andoroid.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+/**
+ * Created by 13512081 - Hendro Triokta Brianto on 08/02/2015.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        Toast.makeText(context, "Don't panik but your time is up!!!.",
+                Toast.LENGTH_LONG).show();
+        // Vibrate the mobile phone
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(2000);
+    }
+
+}
